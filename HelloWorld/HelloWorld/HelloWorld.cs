234192452fad480f2378103a449986d5f470838f
KimSeungDeok <tmdejr1117@gmail.com>
2022-01-03 16:55:19 +0900

    HelloWorld

--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -9,4 +9,11 @@ namespace HelloWorld
             Console.WriteLine("Hello World!");
         }
     }
+    class WorldHello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
 }
