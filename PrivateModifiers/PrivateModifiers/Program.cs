9e307659ff3a25a26adf9ee0e6dc0fb1483ebb49
KimSeungDeok <tmdejr1117@gmail.com>
2022-01-13 15:55:41 +0900

    소멸자

---
+++ PrivateModifiers/PrivateModifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrivateModifiers
+{
+/*    class Test
+    {
+        public void TestMethod()
+        {
+            Program.Main(new string[] { "" });
+        }
+    }*/
+    class Program
+    {
+        class Test
+        {
+            public void TestMethod()
+            {
+                Program.Main(new string[] { "" });
+            }
+        }
+        public void TestMethod()
+        {
+            Program.Main(new string[] { "" });
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
